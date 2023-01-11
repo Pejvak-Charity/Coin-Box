971b28e1aeca903594caff29c223aa3b9ec88a00
ali forghani <a.forghani@sain.ir>
2023-01-11 11:08:16 +0330

    initialize project

---
+++ src/main/java/charity/pejvak/coinbox/CoinBoxApplication.java
@@ -0,0 +1,13 @@
+package charity.pejvak.coinbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinBoxApplication.class, args);
+    }
+
+}
