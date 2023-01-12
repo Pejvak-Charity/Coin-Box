ba1637f82565031996c476a5110f9b714998bc9f
FaGhafff <fatemeh.ghaufori@yahoo.com>
2023-01-12 14:14:35 +0330

    fix bug (add Pageable to custom page selects)

--- src/main/java/charity/pejvak/coinbox/repository/AddressRepository.java
+++ src/main/java/charity/pejvak/coinbox/repository/AddressRepository.java
@@ -5,12 +5,13 @@ import charity.pejvak.coinbox.model.City;
 import charity.pejvak.coinbox.model.User;
 import charity.pejvak.coinbox.model.Zone;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address,Long> {
-    Page<Address> findAllByCity(City city);
-    Page<Address> findAllByCityAndZone(City city, Zone zone);
-    Page<Address> findAllByUser(User user);
+    Page<Address> findAllByCity(City city, Pageable pageable);
+    Page<Address> findAllByCityAndZone(City city, Zone zone,Pageable pageable);
+    Page<Address> findAllByUser(User user,Pageable pageable);
 }
--- src/main/java/charity/pejvak/coinbox/repository/CityRepository.java
+++ src/main/java/charity/pejvak/coinbox/repository/CityRepository.java
@@ -3,11 +3,12 @@ package charity.pejvak.coinbox.repository;
 import charity.pejvak.coinbox.model.City;
 import charity.pejvak.coinbox.model.Province;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CityRepository extends JpaRepository<City,Long> {
-    Page<City> findAllByProvince(Province province);
+    Page<City> findAllByProvince(Province province, Pageable pageable);
 
 }
