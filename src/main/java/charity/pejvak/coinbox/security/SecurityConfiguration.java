fb3e79cba02e0b5dbc1c7e3661928f2dafe07366
ali forghani <a.forghani@sain.ir>
2023-01-12 08:45:45 +0330

    add security configs

---
+++ src/main/java/charity/pejvak/coinbox/security/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package charity.pejvak.coinbox.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+public class SecurityConfiguration {
+
+}
