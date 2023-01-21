63c5e0f6c9ca046f065769913c374da54fb37055
a-forghani-y79 <a.forghani.y79@gmail.com>
2023-01-21 17:34:26 +0330

    debug some configs

--- backend-entrypoint.sh
+++ backend-entrypoint.sh
@@ -3,9 +3,11 @@ APP="/Coin-Box"
 if [ -e "$APP" ]; then
   cd $APP || exit
   git pull
+  mvn spring-boot:start && echo "Back end Start"
 else
   git clone https://github.com/Pejvak-Charity/Coin-Box
   cd $APP || exit
+  mvn spring-boot:stop && echo "Back end Stop"
+  mvn spring-boot:start && echo "Back end Start"
 fi
 
-mvn spring-boot:run && echo "Back end Ready"
